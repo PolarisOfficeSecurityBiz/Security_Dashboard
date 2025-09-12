344537c930ad588b339ad149142473ab4806cd6a
lily-b-lee <lily.b.lee@polarisoffice.com>
2025-09-12 14:23:59 +0900

    init

---
+++ PolarisSecurityDashboard/src/main/java/com/polarisoffice/security/PolarisSecurityDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.polarisoffice.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarisSecurityDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarisSecurityDashboardApplication.class, args);
+	}
+
+}
---
+++ PolarisSecurityDashboard/src/test/java/com/polarisoffice/security/PolarisSecurityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarisoffice.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarisSecurityDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
